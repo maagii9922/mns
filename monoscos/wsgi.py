e2d49d8b5dfb48831a92f52b92952f28843570fc
Enyamdorj <nyamdorj@bumanit.mn>
2021-01-04 11:22:54 +0800

    2

--- monoscos/wsgi.py
+++ monoscos/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for monoscos project.
+WSGI config for monospharma project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/var/www/cosmeticBack')
+sys.path.append('/var/www/cosmeticBack/monoscos')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monoscos.settings.production')
 
 application = get_wsgi_application()
